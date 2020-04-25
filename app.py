a61fc0e24e815c7556a1915a0fced570b7e0aa23
Nicolaus Boby <nicolausboby@gmail.com>
2020-04-25 19:53:06 +0700

    add slider and placeholders

--- app.py
+++ app.py
@@ -1,43 +1,86 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
 import pandas as pd
+from urllib.request import urlopen
+import json
+
+from datetime import datetime
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# Placeholder and constant
+TIME_PLACEHOLDER = [
+    datetime(2019, 10, 12),
+    datetime(2020, 4, 25),
+]
+
+# Initiate server
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+    meta_tags=[
+        {"name": "viewport", "content":"width=device-width, initial-scale=1.0"}
+    ],
+)
+
+server = app.server
+
+# Load Data
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
 
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                dict(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
+
+
+# App Layout
+
+app.layout = html.Div(
+    id="root",
+    children=[
+        html.Div(
+            id="header",
+            children=[
+                html.P(
+                    id="description",
+                    children="Covid-19 dashboard",
+                ),
             ],
-            'layout': dict(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
-])
+        ),
+        html.Div(
+            id="app-container",
+            children=[
+                html.Div(
+                    id="top-section",
+                    children=[
+                        html.P(
+                            id="slider-text",
+                            children="Drag the slider to change the time"
+                        ),
+                        dcc.Slider(
+                            id="time-slider",
+                            min=min(TIME_PLACEHOLDER),
+                            max=max(TIME_PLACEHOLDER),
+                            value=min(TIME_PLACEHOLDER),
+                            marks={
+                                str(time.strftime("%m/%d/%Y")): {
+                                    "label": str(time.strftime("%m/%d/%Y")),
+                                    "style": {"color": "#7fafdf"},
+                                }
+                                for time in TIME_PLACEHOLDER
+                            },
+                        ),
+                    ],
+                ),
+                html.Div(
+                    
+                )
+            ],
+        ),
+    ],
+)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
